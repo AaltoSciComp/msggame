2bd70ce40278cb703f3a28cfc5090d1c68d43e52
Richard Darst <rkd@zgib.net>
2019-11-17 16:51:43 +0200

    Add django model admin interface

--- msggame/admin.py
+++ msggame/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models
+
+class PersonAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.Person, PersonAdmin)
+
+class LinkAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.Link, LinkAdmin)
+
+class MessageAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.Message, MessageAdmin)
+
+class RelayAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.Relay, RelayAdmin)
+
+class GameAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.Game, GameAdmin)
