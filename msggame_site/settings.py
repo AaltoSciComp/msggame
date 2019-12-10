fd2d66841744a65eb5205c77948d2854fc0c73e1
Richard Darst <rkd@zgib.net>
2019-12-10 23:08:22 +0200

    msggame_site/settings.py: reorder the settings_local loading

--- msggame_site/settings.py
+++ msggame_site/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-exec(open(os.path.join(os.path.dirname(__file__), 'settings_local.py')).read())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+exec(open(os.path.join(os.path.dirname(__file__), 'settings_local.py')).read())
